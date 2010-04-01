87c85a6449385b1256e0c64bbf5369c4faf9746d
Perry Hung <iperry@alum.mit.edu>
2010-03-31 22:22:29 -0400

    Licensing: Relicensed libmaple under the MIT License.
    Relicensed the bulk of libmaple under the more permissive MIT License,
    from GPLv3. Files that were largely or entirely derived from
    the Arduino, STM, or Lanchon retain their original licenses.

--- core/wiring_math.cpp
+++ core/wiring_math.cpp
@@ -1,15 +1,31 @@
+/*
+  Modified by LeafLabs, LLC.
+
+  Part of the Wiring project - http://wiring.org.co
+  Copyright (c) 2004-06 Hernando Barragan
+  Modified 13 August 2006, David A. Mellis for Arduino - http://www.arduino.cc/
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General
+  Public License along with this library; if not, write to the
+  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+  Boston, MA  02111-1307  USA
+
+  $Id$
+*/
+
 #include <stdlib.h>
 #include "math.h"
 
-/* from newlib:
- *
- * rand returns the next pseudo-random integer in sequence; it is a number 
- *      between 0 and RAND_MAX (inclusive).
- *
- * srand does not return a result. */
-
-
-/* The rest copied from WMath.cpp  */
 void randomSeed(unsigned int seed) {
   if (seed != 0) {
     srand(seed);

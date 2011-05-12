f92d5f4865304cb79760dc1826bca117e98f7757
Marti Bolivar <mbolivar@leaflabs.com>
2011-05-12 04:37:30 -0400

    Fixing HardwareSPI.h mistake for non-LeafLabs boards.
    "#warn" -> "#warning"; oops.

--- wirish/comm/HardwareSPI.h
+++ wirish/comm/HardwareSPI.h
@@ -59,7 +59,7 @@ typedef enum SPIFrequency {
 
 #if CYCLES_PER_MICROSECOND != 72
 /* TODO [0.2.0?] something smarter than this */
-#warn "Unexpected clock speed; SPI frequency calculation will be incorrect"
+#warning "Unexpected clock speed; SPI frequency calculation will be incorrect"
 #endif
 
 /**

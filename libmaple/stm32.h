cdb8e265bf5650236b8d1a8ea72a7614b2534b0d
Marti Bolivar <mbolivar@leaflabs.com>
2011-06-11 19:55:40 -0400

    stm32.h: Don't blindly assume PCLK1 and PCLK2 values.
    Put the PCLK1 and PCLK2 within #ifndef guards.  This assumption is
    biased towards LeafLabs boards.

--- libmaple/stm32.h
+++ libmaple/stm32.h
@@ -5,8 +5,12 @@
 #ifndef _STM32_H_
 #define _STM32_H_
 
+#ifndef PCLK1
 #define PCLK1   36000000U
+#endif
+#ifndef PCLK2
 #define PCLK2   72000000U
+#endif
 
 #ifdef STM32_MEDIUM_DENSITY
     #define NR_INTERRUPTS 43

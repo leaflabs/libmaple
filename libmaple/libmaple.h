9dde5488a4816842189a23d9d91c6cb44fbc80f9
Marti Bolivar <mbolivar@leaflabs.com>
2011-07-18 14:07:17 -0400

    Move STM32 config from libmaple.h into stm32.h.

--- libmaple/libmaple.h
+++ libmaple/libmaple.h
@@ -46,44 +46,5 @@
 #define USER_ADDR_RAM 0x20000C00
 #define STACK_TOP     0x20000800
 
-/* MCU-specific configuration */
-#if defined(MCU_STM32F103RB)
-    /* e.g., LeafLabs Maple */
-
-    /* Number of GPIO ports (GPIOA, GPIOB, etc.) */
-    #define NR_GPIO_PORTS              4
-
-    /* SRAM size, in bytes */
-    #define SRAM_SIZE             0x5000
-
-#elif defined(MCU_STM32F103ZE)
-    /* e.g., LeafLabs Maple Native */
-
-    #define NR_GPIO_PORTS              7
-    #define SRAM_SIZE            0x10000
-
-#elif defined(MCU_STM32F103CB)
-    /* e.g., LeafLabs Maple Mini */
-
-    /* Note that this is not, strictly speaking, true.  But only pins
-       0 and 1 exist, and they're used for OSC on the Mini, so we'll
-       live with this for now. */
-    #define NR_GPIO_PORTS              3
-
-    #define SRAM_SIZE             0x5000
-
-#elif defined(MCU_STM32F103RE)
-    /* e.g., LeafLabs Maple RET6 edition */
-
-    #define NR_GPIO_PORTS              4
-    #define SRAM_SIZE            0x10000
-
-#else
-
-#error "No MCU type specified. Add something like -DMCU_STM32F103RB "   \
-       "to your compiler arguments (probably in a Makefile)."
-
-#endif
-
 #endif
 

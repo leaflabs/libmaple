28c5b81f6dd90ce560251e72fdb1250133bddbb6
Marti Bolivar <mbolivar@leaflabs.com>
2011-09-06 22:36:43 -0400

    util.c: Respect stm32.h.

--- libmaple/util.c
+++ libmaple/util.c
@@ -39,7 +39,7 @@
 /* Failed ASSERT()s send out a message using this USART config. */
 #ifndef ERROR_USART
 #define ERROR_USART            USART2
-#define ERROR_USART_CLK_SPEED  72000000UL
+#define ERROR_USART_CLK_SPEED  PCLK1
 #define ERROR_USART_BAUD       9600
 #define ERROR_TX_PORT          GPIOA
 #define ERROR_TX_PIN           2

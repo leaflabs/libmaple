fc3fb928fa780ddb0053db675e79f25c9105a97b
Marti Bolivar <mbolivar@leaflabs.com>
2012-05-11 16:45:38 -0400

    libmaple/nvic.h: Doxygen tweak.s
    Signed-off-by: Marti Bolivar <mbolivar@leaflabs.com>

--- libmaple/include/libmaple/nvic.h
+++ libmaple/include/libmaple/nvic.h
@@ -56,17 +56,29 @@ extern "C"{
 /** NVIC register map type. */
 typedef struct nvic_reg_map {
     __io uint32 ISER[8];      /**< Interrupt Set Enable Registers */
-    uint32 RESERVED0[24];     /**< Reserved */
+    /** Reserved */
+    uint32 RESERVED0[24];
+
     __io uint32 ICER[8];      /**< Interrupt Clear Enable Registers */
-    uint32 RESERVED1[24];     /**< Reserved */
+    /** Reserved */
+    uint32 RESERVED1[24];
+
     __io uint32 ISPR[8];      /**< Interrupt Set Pending Registers */
-    uint32 RESERVED2[24];     /**< Reserved */
+    /** Reserved */
+    uint32 RESERVED2[24];
+
     __io uint32 ICPR[8];      /**< Interrupt Clear Pending Registers */
-    uint32 RESERVED3[24];     /**< Reserved */
+    /** Reserved */
+    uint32 RESERVED3[24];
+
     __io uint32 IABR[8];      /**< Interrupt Active bit Registers */
-    uint32 RESERVED4[56];     /**< Reserved */
+    /** Reserved */
+    uint32 RESERVED4[56];
+
     __io uint8  IP[240];      /**< Interrupt Priority Registers */
-    uint32 RESERVED5[644];    /**< Reserved */
+    /** Reserved */
+    uint32 RESERVED5[644];
+
     __io uint32 STIR;         /**< Software Trigger Interrupt Registers */
 } nvic_reg_map;
 

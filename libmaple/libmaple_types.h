209fe92cd594531f73008f4ac5384bdfc345a60d
Rei Vilo <rei_vilo@yahoo.fr>
2012-06-15 21:03:19 +0300

    #include <stdint.h> for standard integer types in wirish/wirish.h

--- libmaple/libmaple_types.h
+++ libmaple/libmaple_types.h
@@ -43,20 +43,6 @@ typedef short int16;
 typedef int int32;
 typedef long long int64;
 
-///
-/// @brief  [u]int{8|16|32|64}_t types added
-/// @note	Rei VILO, 2012-06-09
-///
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
-
-typedef signed char int8_t;
-typedef short int16_t;
-typedef int int32_t;
-typedef long long int64_t;
-
 typedef void (*voidFuncPtr)(void);
 
 #define __io volatile

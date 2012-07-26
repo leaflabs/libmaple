7c976e4e5d9afd8aaf1a88b8765a89b4affcc665
Marti Bolivar <mbolivar@leaflabs.com>
2012-07-26 12:55:02 -0400

    Hack: Provide a hook so Mini doesn't disable JTAG/SWD.
    The nonexistence of a configuration system is starting to become kind
    of a pain. Oh well; let's keep piling hacks on until things break.
    
    Signed-off-by: Marti Bolivar <mbolivar@leaflabs.com>

--- wirish/boards/maple_mini/board.cpp
+++ wirish/boards/maple_mini/board.cpp
@@ -39,9 +39,11 @@
 #include <wirish/wirish_types.h>
 
 /* Since we want the Serial Wire/JTAG pins as GPIOs, disable both SW
- * and JTAG debug support */
+ * and JTAG debug support, unless configured otherwise. */
 void boardInit(void) {
+#ifndef CONFIG_MAPLE_MINI_NO_DISABLE_DEBUG
     disableDebugPorts();
+#endif
 }
 
 extern const stm32_pin_info PIN_MAP[BOARD_NR_GPIO_PINS] = {

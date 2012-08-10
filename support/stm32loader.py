f2aaed8818580663f036bc3d89296f9491310359
Marti Bolivar <mbolivar@leaflabs.com>
2012-08-10 17:20:39 -0400

    stm32loader.py: replace obsolete exception catch syntax.
    For Python 3 support.
    
    Signed-off-by: Marti Bolivar <mbolivar@leaflabs.com>

--- support/stm32loader.py
+++ support/stm32loader.py
@@ -422,7 +422,7 @@ if __name__ == "__main__":
 
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hqVewvrp:b:a:l:")
-    except getopt.GetoptError, err:
+    except getopt.GetoptError as err:
         # print help information and exit:
         print(str(err)) # will print something like "option -a not recognized"
         usage()

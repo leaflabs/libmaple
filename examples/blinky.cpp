e58051ea460ded35b0ea1cbc769ebeb6dab69ffd
Marti Bolivar <mbolivar@leaflabs.com>
2013-01-01 20:11:14 -0500

    examples/blinky.cpp: Use toggleLED() instead of digitalWrite().
    Keep things simple.
    
    Signed-off-by: Marti Bolivar <mbolivar@leaflabs.com>

--- examples/blinky.cpp
+++ examples/blinky.cpp
@@ -6,18 +6,11 @@ void setup() {
     pinMode(BOARD_LED_PIN, OUTPUT);
 }
 
-int toggle = 1;
-
 void loop() {
-    // You could just use toggleLED() instead, but this illustrates
-    // the use of digitalWrite():
-    digitalWrite(BOARD_LED_PIN, toggle);
-    toggle ^= 1;
+    togglePin(BOARD_LED_PIN);
     delay(100);
 }
 
-// Force init to be called *first*, i.e. before static object allocation.
-// Otherwise, statically allocated objects that need libmaple may fail.
 __attribute__((constructor)) void premain() {
     init();
 }

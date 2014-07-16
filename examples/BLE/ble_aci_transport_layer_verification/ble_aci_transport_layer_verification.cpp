79dcffb0098a6595745671d23a582e293cb01d77
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2014-07-16 16:53:30 +0200

    Ported transport layer verification example

--- examples/BLE/ble_aci_transport_layer_verification/ble_aci_transport_layer_verification.cpp
+++ examples/BLE/ble_aci_transport_layer_verification/ble_aci_transport_layer_verification.cpp
@@ -37,8 +37,8 @@ received in the ACI echo event should be the same.
  */
 
 #define DEBUG_ENABLE CODED_TRACES
-#include <SPI.h>
-#include <lib_aci.h>
+#include <wirish/wirish.h>
+#include <BLE/lib_aci.h>
 
 // aci_struct that will contain
 // total initial credits
@@ -63,52 +63,50 @@ static uint8_t aci_echo_cmd = 0;
 /* Define how assert should function in the BLE library */
 void __ble_assert(const char *file, uint16_t line)
 {
-  Serial.print("ERROR ");
-  Serial.print(file);
-  Serial.print(": ");
-  Serial.print(line);
-  Serial.print("\n");
+  SerialUSB.print("ERROR ");
+  SerialUSB.print(file);
+  SerialUSB.print(": ");
+  SerialUSB.print(line);
+  SerialUSB.print("\n");
   while(1);
 }
 
 void setup(void)
 {
-  Serial.begin(115200);
-  //Wait until the serial port is available (useful only for the Leonardo)
-  //As the Leonardo board is not reseted every time you open the Serial Monitor
-  #if defined (__AVR_ATmega32U4__)
-    while(!Serial)
-    {}
-    delay(5000);  //5 seconds delay for enabling to see the start up comments on the serial board
-  #elif defined(__PIC32MX__)
-    delay(1000);
-  #endif
-  Serial.println(F("Arduino setup"));
+  Serial2.begin(115200);
+  //Wait until the serial port is available 
+  delay(1000);
+
+
+  SerialUSB.println(F("Maple setup"));
+  SerialUSB.println(F("Set line ending to newline to send data from the serial monitor"));
 
   /*
   Tell the ACI library, the MCU to nRF8001 pin connections.
   The Active pin is optional and can be marked UNUSED
   */
   aci_state.aci_pins.board_name = BOARD_DEFAULT; //See board.h for details REDBEARLAB_SHIELD_V1_1 or BOARD_DEFAULT
-  aci_state.aci_pins.reqn_pin   = 9; //SS for Nordic board, 9 for REDBEARLAB_SHIELD_V1_1
-  aci_state.aci_pins.rdyn_pin   = 8; //3 for Nordic board, 8 for REDBEARLAB_SHIELD_V1_1
-  aci_state.aci_pins.mosi_pin   = MOSI;
-  aci_state.aci_pins.miso_pin   = MISO;
-  aci_state.aci_pins.sck_pin    = SCK;
+  aci_state.aci_pins.reqn_pin   = 9; 
+  aci_state.aci_pins.rdyn_pin   = 8;
+  aci_state.aci_pins.mosi_pin   = BOARD_SPI1_MOSI_PIN;
+  aci_state.aci_pins.miso_pin   = BOARD_SPI1_MISO_PIN;
+  aci_state.aci_pins.sck_pin    = BOARD_SPI1_SCK_PIN;
 
-  aci_state.aci_pins.spi_clock_divider      = SPI_CLOCK_DIV8;//SPI_CLOCK_DIV8  = 2MHz SPI speed
-                                                             //SPI_CLOCK_DIV16 = 1MHz SPI speed
-
-  aci_state.aci_pins.reset_pin              = 4; //4 for Nordic board, UNUSED for REDBEARLAB_SHIELD_V1_1
+  aci_state.aci_pins.spi_clock_divider      = SPI_2_25MHZ;
+  
+  aci_state.aci_pins.reset_pin              = 7; 
   aci_state.aci_pins.active_pin             = UNUSED;
   aci_state.aci_pins.optional_chip_sel_pin  = UNUSED;
 
-  aci_state.aci_pins.interface_is_interrupt = false;
+  aci_state.aci_pins.interface_is_interrupt = false; //Interrupts disabled for Maple board
   aci_state.aci_pins.interrupt_number       = 1;
 
+  //We reset the nRF8001 here by toggling the RESET line connected to the nRF8001
+  //If the RESET line is not available we call the ACI Radio Reset to soft reset the nRF8001
+  //then we initialize the data structures required to setup the nRF8001
   //The second parameter is for turning debug printing on for the ACI Commands and Events so they be printed on the Serial
-  hal_aci_tl_init(&(aci_state.aci_pins),false);
-  Serial.println(F("nRF8001 Reset done"));
+  lib_aci_init(&aci_state, false);
+  SerialUSB.println(F("Set up done"));
 }
 
 void loop()
@@ -129,19 +127,19 @@ void loop()
         switch(aci_evt->params.device_started.device_mode)
         {
           case ACI_DEVICE_SETUP:
-            Serial.println(F("Evt Device Started: Setup"));
+            SerialUSB.println(F("Evt Device Started: Setup"));
             lib_aci_test(ACI_TEST_MODE_DTM_UART);
             break;
           case ACI_DEVICE_STANDBY:
-            Serial.println(F("Evt Device Started: Standby"));
+            SerialUSB.println(F("Evt Device Started: Standby"));
             break;
           case ACI_DEVICE_TEST:
           {
             uint8_t i = 0;
-            Serial.println(F("Evt Device Started: Test"));
-            Serial.println(F("Started infinite Echo test"));
-            Serial.println(F("Repeat the test with all bytes in echo_data inverted."));
-            Serial.println(F("Waiting 4 seconds before the test starts...."));
+            SerialUSB.println(F("Evt Device Started: Test"));
+            SerialUSB.println(F("Started infinite Echo test"));
+            SerialUSB.println(F("Repeat the test with all bytes in echo_data inverted."));
+            SerialUSB.println(F("Waiting 4 seconds before the test starts...."));
             delay(4000);
             for(i=0; i<NUM_ECHO_CMDS; i++)
             {
@@ -160,20 +158,20 @@ void loop()
           //ACI ReadDynamicData and ACI WriteDynamicData will have status codes of
           //TRANSACTION_CONTINUE and TRANSACTION_COMPLETE
           //all other ACI commands will have status code of ACI_STATUS_SCUCCESS for a successful command
-          Serial.print(F("ACI Command 0x"));
-          Serial.println(aci_evt->params.cmd_rsp.cmd_opcode, HEX);
-          Serial.println(F("Evt Cmd respone: Error. Arduino is in an while(1); loop"));
+          SerialUSB.print(F("ACI Command 0x"));
+          SerialUSB.println(aci_evt->params.cmd_rsp.cmd_opcode, HEX);
+          SerialUSB.println(F("Evt Cmd respone: Error. Maple Board is in an while(1); loop"));
           while (1);
         }
         break;
       case ACI_EVT_ECHO:
         if (0 != memcmp(&echo_data[0], &(aci_evt->params.echo.echo_data[0]), sizeof(echo_data)))
         {
-          Serial.println(F("Error: Echo loop test failed. Verify the SPI connectivity on the PCB."));
+          SerialUSB.println(F("Error: Echo loop test failed. Verify the SPI connectivity on the PCB."));
         }
         else
         {
-          Serial.println(F("Echo OK"));
+          SerialUSB.println(F("Echo OK"));
         }
         if (NUM_ECHO_CMDS == aci_echo_cmd)
         {
@@ -191,8 +189,26 @@ void loop()
   else
   {
     // No event in the ACI Event queue
-    // Arduino can go to sleep now
+    // Maple Board can go to sleep now
     // Wakeup from sleep from the RDYN line
   }
+}
+
+// -- init() and main() -----------------------------------------
+
+__attribute__((constructor)) void premain()
+{
+  init();
+}
+
+int main(void)
+{
+  setup();
+
+  while (true)
+  {
+    loop();
+  }
+  return 0;
 }
 

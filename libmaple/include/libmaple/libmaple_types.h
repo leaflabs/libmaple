32b6da1f837a0e952432438b0448dee8e20d4cb4
Marti Bolivar <mbolivar@leaflabs.com>
2014-10-17 16:47:52 +0800

    libmaple_types.h: add ifndef guards for some macros
    Signed-off-by: Marti Bolivar <mbolivar@leaflabs.com>

--- libmaple/include/libmaple/libmaple_types.h
+++ libmaple/include/libmaple/libmaple_types.h
@@ -55,8 +55,12 @@ typedef void (*voidArgumentFuncPtr)(void *);
 #define __packed __attribute__((__packed__))
 #define __deprecated __attribute__((__deprecated__))
 #define __weak __attribute__((weak))
+#ifndef __always_inline
 #define __always_inline inline __attribute__((always_inline))
+#endif
+#ifndef __unused
 #define __unused __attribute__((unused))
+#endif
 
 #ifndef NULL
 #define NULL 0
